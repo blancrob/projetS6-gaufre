9489bf03c786d8bfb59d9dbdd5e3386b24c252d8
Antoine <antoine26.guerin@gmail.com>
2018-04-27 09:24:53 +0200

    correction aiNonCoupPerdant

--- gaufre/AI.java
+++ gaufre/AI.java
@@ -28,8 +28,8 @@ public class AI {
     }
     
     /**
-     * 
-     * @return
+     * init : void
+     * @return un point p choisit de manière aléatoire, hors poison si possible, parmis les cases possibles.
      */
     public Point aiAleatoire(){
         Point p = new Point();
@@ -49,6 +49,10 @@ public class AI {
         return p;
     }
     
+    /**
+     * 
+     * @return un point p permettant de gagner, null sinon.
+     */
     public Point aiCoupGagnant(){
         Point p = new Point();
         if (!plateau[0][1] && plateau[1][0]){
@@ -60,10 +64,13 @@ public class AI {
         return p;
     }
     
+    /**
+     * 
+     * @return un point p permettant de gagner si possible ou de ne pas perdre sinon.
+     */
     public Point aiNonCoupPerdant(){
-        Point p = new Point();
-        p = this.aiCoupGagnant();
-        if ( p==null ){
+        Point p = this.aiCoupGagnant();
+        if ( p.getX() == 0 && p.getY() == 0 ){
             p=this.aiAleatoire();
             if(plateau[0][1] && plateau[1][0] && (plateau[2][0] || plateau[1][1] || plateau[0][2])){
                while((p.getX() <= 1 && p.getY()==0) || (p.getX() ==0 && p.getY() <=1) ){
